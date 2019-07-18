e693178a8802ffeb4210982b1a09d61b8f9d1c37
MattyGam3r <53043544+MattyGam3r@users.noreply.github.com>
2019-07-19 09:24:18 +0930

    Initial commit

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour
+{
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void Instructions()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Square.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Square : MonoBehaviour
+{
+    [SerializeField] GameObject Box1;
+    [SerializeField] GameObject Box2;
+    [SerializeField] Text Score1;
+    [SerializeField] Text Score2;
+    [SerializeField] bool IsGameFinished;
+    [SerializeField] GameObject Player1Win;
+    [SerializeField] GameObject Player2Win;
+    [SerializeField] GameObject PlayAgain;
+    int Number1;
+    int Number2;
+    public void Grow1()
+    {
+        Box1.transform.localScale += new Vector3(.04f, .04f, 0);
+        Debug.Log("W key has been pressed ^_^");
+    }
+
+    public void Grow2()
+    {
+        Box2.transform.localScale += new Vector3(.04f, .04f, 0);
+        Debug.Log("O key has been pressed ^_^");
+    }
+
+    public void CheckWinner1()
+    {
+        if (Number1 >= 100)
+        {
+            IsGameFinished = true;
+            Player1Win.SetActive(true);
+            PlayAgain.SetActive(true);
+            Debug.Log("Player1 Wins");
+        }
+        else return;
+    }
+
+    public void CheckWinner2()
+    {
+        if (Number2 >= 100)
+        {
+            IsGameFinished = true;
+            Player2Win.SetActive(true);
+            PlayAgain.SetActive(true);
+            Debug.Log("Player2 Wins");
+        }
+        else return;
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int Number1 = 1;
+        int Number2 = 1;
+        IsGameFinished = false;
+        Player1Win.SetActive(false);
+        Player2Win.SetActive(false);
+        PlayAgain.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (IsGameFinished == false)
+            {
+                Grow1();
+                Number1 += 1;
+                Score1.text = Number1.ToString();
+                CheckWinner1();
+                
+            }
+            else { return; }
+        }
+
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            if (IsGameFinished == false)
+            {
+                Grow2();
+                Number2 += 1;
+                Score2.text = Number2.ToString();
+                CheckWinner2();
+
+            }
+            else { return; }
+        }
+
+
+    }
+
+}
